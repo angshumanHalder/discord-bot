64083a9047a99e7e7b826be06f8411d489748b6e
angshuman123456 <angshuman.halder243@gmail.com>
2020-10-04 23:56:55 +0530

    added helper methods and tests for singly linked list

--- src/code/data-structures/SinglyLinkedList/SinglyLinkedList.py
+++ src/code/data-structures/SinglyLinkedList/SinglyLinkedList.py
@@ -1,18 +1,20 @@
-# Node class
-class Node:
-  def __init__(self, value):
-      self.value = value
-      self.next = None
-
 # Linked List Class
 class SinglyLinkedList:
+    """ Private Node Class """
+    class _Node:
+        def __init__(self, value):
+            self.value = value
+            self.next = None
+        def __str__(self):
+            return str(self.value)
+
     def __init__(self):
         self.head = None
         self.tail = None
         self.length = 0
 
     def push_beginning(self, value):
-        node = Node(value)
+        node = self._Node(value)
         if self.length == 0:
             self.head = node
             self.tail = node
@@ -20,9 +22,10 @@ class SinglyLinkedList:
             node.next = self.head
             self.head = node
         self.length += 1
+        return True
 
     def push_end(self, value):
-        node = Node(value)
+        node = self._Node(value)
         if self.length == 0:
             self.head = node
             self.tail = node
@@ -30,14 +33,19 @@ class SinglyLinkedList:
             self.tail.next = node
             self.tail = node
         self.length += 1
+        return True
 
     def push_at_index(self, value, index):
+        if self._is_empty() and index != 0:
+            raise IndexError("List is empty")
+        self._is_out_of_bounds(index)
+        
         if index == 0:
             self.push_beginning(value)
         elif index >= self.length - 1:
             self.push_end(value)
         else:
-            node = Node(value)
+            node = self._Node(value)
             i = 0
             temp_node = self.head
             while i < index - 1:
@@ -46,10 +54,12 @@ class SinglyLinkedList:
             node.next = temp_node.next
             temp_node.next = node
             self.length += 1
+            return True
 
     def remove_beginning(self):
-        if self.length == 0:
+        if self._is_empty():
             raise IndexError("List is empty")
+
         value = self.head.value
         temp_node = self.head
         self.head = self.head.next
@@ -58,8 +68,9 @@ class SinglyLinkedList:
         return value
 
     def remove_end(self):
-        if self.length == 0:
+        if self._is_empty():
             raise IndexError("List is empty")
+
         value = self.tail.value
         temp_node = self.head
         while temp_node.next.next is not None:
@@ -70,8 +81,10 @@ class SinglyLinkedList:
         return value
 
     def remove_at_index(self, index):
-        if self.length == 0:
+        if self._is_empty():
             raise IndexError("List is empty")
+        self._is_out_of_bounds(index)
+
         if index == 0:
             value = self.remove_beginning()
         elif index >= self.length - 1:
@@ -90,10 +103,10 @@ class SinglyLinkedList:
         return value
 
     def get_value_at(self, index):
-        if self.length == 0:
+        if self._is_empty():
             raise IndexError("List is empty")
-        if index >= self.length:
-            raise IndexError(f"No node at index {index}")
+        self._is_out_of_bounds(index)
+
         i = 0
         temp_node = self.head
         while i < index:
@@ -102,16 +115,17 @@ class SinglyLinkedList:
         return temp_node.value
 
     def set_value_at(self, value, index):
-        if self.length == 0:
+        if self._is_empty():
             raise IndexError("List is empty")
-        if index >= self.length:
-            raise IndexError(f"No node at index {index}")
+        self._is_out_of_bounds(index)
+        
         i = 0
         temp_node = self.head
         while i < index:
             temp_node = temp_node.next
             i += 1
         temp_node.value = value
+        return True
 
     def reverse_list(self):
         temp_node = self.head
@@ -126,12 +140,27 @@ class SinglyLinkedList:
             prev_node = temp_node
             temp_node = next_node
             i += 1
+        return True
+
+    """ Helper methods """
+    def size(self):
+        return self.length
+    
+    def _is_empty(self):
+        return self.length == 0
+
+    def _is_out_of_bounds(self, idx):
+        if idx >= self.length:
+            raise IndexError('Index out of bounds')
 
     def __str__(self):
         temp_node = self.head
-        lst_str = ""
+        lst_str = "["
         while temp_node is not None:
-            lst_str += str(temp_node.value) + ", "
+            lst_str += str(temp_node.value)
+            if temp_node.next is not None:
+                lst_str += ","
             temp_node = temp_node.next
+        lst_str += "]"
         return lst_str
   
---
+++ src/code/data-structures/SinglyLinkedList/SinglyLinkedList.test.py
@@ -0,0 +1,19 @@
+from SinglyLinkedList import SinglyLinkedList
+
+linkedList = SinglyLinkedList()
+assert linkedList.push_beginning(1) is True
+assert linkedList.push_beginning(2) is True
+assert linkedList.push_end(3) is True
+assert linkedList.push_end(4) is True
+assert linkedList.push_end(5) is True
+assert linkedList.push_at_index(80, 3) is True
+assert linkedList.__str__() == "[2,1,3,80,4,5]"
+assert linkedList.remove_beginning() == 2
+assert linkedList.remove_end() == 5
+assert linkedList.remove_at_index(2) == 80
+assert linkedList.get_value_at(2) == 4
+assert linkedList.set_value_at(20, 0) is True
+assert linkedList.push_end(40) is True
+assert linkedList.__str__() == "[20,3,4,40]"
+assert linkedList.reverse_list() is True
+assert linkedList.__str__() == "[40,4,3,20]"
