2107e88831232fdd482c3123e20fce4eb71adce3
Angshuman <angshuman@pop-os.localdomain>
2020-10-04 23:08:49 +0530

    test to list servers and channels that the bot is connected to

--- src/tests/test_bot.ts
+++ src/tests/test_bot.ts
@@ -6,10 +6,16 @@ const client = new Discord.Client();
 client.on("ready", () => {
   // Check if the bot is connected
   console.log(`Connected as ${client.user?.tag}`);
+
   // List all the servers(guilds) the bot is connected to
   console.log("Servers: ");
   client.guilds.cache.forEach((guild: Discord.Guild): void => {
     console.log(`- ${guild.name}`);
+  });
+
+  // List all the channels
+  client.channels.cache.forEach((channel): void => {
+    console.log(`--${channel.toString()} (${channel.type}) - ${channel.id}`);
   });
 });
 
